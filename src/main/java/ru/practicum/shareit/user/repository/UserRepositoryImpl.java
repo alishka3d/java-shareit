ad1d936f11b5e939a49d98d36fc8a5bdde3f6ce7
alishka3d <alishka3d@yandex.ru>
2022-12-23 23:24:46 +0300

    feat: add method update() and delete()

--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -3,6 +3,8 @@ package ru.practicum.shareit.user.repository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.exceptions.EntityAlreadyExistException;
+import ru.practicum.shareit.exceptions.EntityNotFoundException;
+import ru.practicum.shareit.exceptions.ValidationException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.*;
@@ -41,16 +43,48 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User findById(Long id) {
-        return null;
+        if (users.containsKey(id)) {
+            log.info("Пользоватеь с id {} найден.", id);
+            return users.get(id);
+        } else {
+            log.error("Пользователь с id {} не найден.", id);
+            throw new EntityNotFoundException("Пользователь с id " + id + " не найден.");
+        }
     }
 
     @Override
     public User update(Long id, User user) {
-        return null;
+        if (users.containsKey(id)) {
+            if (id != user.getId() && users.containsKey(user.getId())) {
+                log.error("Ошибка обновления пользователя. id {} занят.", user.getId());
+                throw new EntityAlreadyExistException("Ошибка обновления пользователя. id " + id + " занят.");
+            } else if (user.getEmail() == null) {
+                log.error("Ошибка обновления пользователя. Email = null");
+                throw new ValidationException("Ошибка обновления пользователя. Email = null");
+            } else if (!users.get(id).getEmail().equals(user.getEmail()) && emails.contains(user.getEmail())) {
+                log.error("Ошибка обновления пользователя. email {} занят.", user.getEmail());
+                throw new EntityAlreadyExistException("Ошибка обновления пользователя. email " + user.getEmail() + " занят.");
+            } else {
+                log.info("Пользователь с id {} обновлен.", id);
+                emails.add(user.getEmail());
+                users.remove(id);
+                users.put(user.getId(), user);
+                return users.get(user.getId());
+            }
+        }
+        log.error("Пользователь с id {} не найден.", id);
+        throw new EntityNotFoundException("Пользователь с id " + id + " не найден.");
     }
 
     @Override
     public void delete(Long id) {
-
+        if (users.containsKey(id)) {
+            emails.remove(users.get(id).getEmail());
+            log.info("Пользователь с id {} удалён", id);
+            users.remove(id);
+        } else {
+            log.error("Пользователь с id {} не найден.", id);
+            throw new EntityNotFoundException("Пользователь с id " + id + " не найден.");
+        }
     }
 }
