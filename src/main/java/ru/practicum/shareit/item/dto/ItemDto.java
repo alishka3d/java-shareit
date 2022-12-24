d4ec915eeb527bc27b51ed0ad1c08834a8f99591
alishka3d <alishka3d@yandex.ru>
2022-12-24 17:29:46 +0300

    feat: add classes Item, ItemDto, ItemMapper

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,27 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
 public class ItemDto {
+
+    @Positive
+    private long id;
+    @NotBlank(message = "Название предмета не может быть пустым.")
+    private String name;
+    @NotBlank(message = "Описание предмета не может быть пустым.")
+    private String description;
+    private Boolean available;
+    private User owner;
+    private ItemRequest request;
 }
---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.item.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+@Component
+public class ItemMapper {
+
+    public static Item toItem(ItemDto itemDto) {
+        return new Item(
+                itemDto.getId(),
+                itemDto.getName(),
+                itemDto.getDescription(),
+                itemDto.getAvailable(),
+                itemDto.getOwner(),
+                itemDto.getRequest()
+        );
+    }
+
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable(),
+                item.getOwner(),
+                item.getRequest()
+        );
+    }
+}
