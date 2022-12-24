0a175da89cb3029f582055bfc1fc0147182c198d
alishka3d <alishka3d@yandex.ru>
2022-12-25 00:56:28 +0300

    feat: add class ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.user.model.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 /**
  * TODO Sprint add-item-requests.
  */
+
+@Data
+@AllArgsConstructor
 public class ItemRequest {
+
+    @Positive
+    private Long id;
+    @NotBlank(message = "Описание не может быть пустым.")
+    private String description;
+    private User requestor;
 }
