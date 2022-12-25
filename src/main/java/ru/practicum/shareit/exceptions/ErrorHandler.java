dcb0fc5e3efa84d9bd5d32e6014fc1be310a60e4
alishka3d <alishka3d@yandex.ru>
2022-12-26 02:24:40 +0300

    feat: add classes ErrorHandler, ErrorResponse

---
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIncorrectParameterException(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleEntityNotFoundException(final EntityNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleEntityAlreadyExistException(final EntityAlreadyExistException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+}
